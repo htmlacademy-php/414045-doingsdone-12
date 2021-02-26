2a145dc3ec676037cb77fb11c213eee47330a484
Kuzmin Roman <archer039@gmail.com>
2021-02-26 16:24:57 +0200

    изменил названия полей, добавил связи

--- schema.sql
+++ schema.sql
@@ -6,21 +6,29 @@ USE doingsdone;
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    dt_reg TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     email VARCHAR(128) NOT NULL UNIQUE,
-    name VARCHAR(64) NOT NULL UNIQUE,
+    name VARCHAR(64) NOT NULL,
     password VARCHAR(64) NOT NULL
 );
 
 CREATE INDEX u_email ON users(email);
 
+CREATE TABLE projects (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT REFERENCES users(id),
+    title VARCHAR(64) NOT NULL
+);
+
+CREATE INDEX p_user_id ON projects(user_id);
+
 CREATE TABLE tasks (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT(64) NOT NULL,
-    project_id INT(64) NOT NULL,
-    dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INT REFERENCES users(id),
+    project_id INT REFERENCES projects(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     is_done INT(1) DEFAULT 0,
-    task VARCHAR(64) NOT NULL,
+    title VARCHAR(64) NOT NULL,
     file_src VARCHAR(128),
     time_end TIMESTAMP NOT NULL
 );
@@ -28,10 +36,3 @@ CREATE TABLE tasks (
 CREATE INDEX t_user_id ON tasks(user_id);
 CREATE INDEX t_project_id ON tasks(project_id);
 
-CREATE TABLE projects (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id VARCHAR(64) NOT NULL,
-    project_name VARCHAR(64) NOT NULL
-);
-
-CREATE INDEX p_user_id ON projects(user_id);
