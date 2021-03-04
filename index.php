07951b2f657fb5d99fa1e3c2543914ac9d993965
Kuzmin Roman <archer039@gmail.com>
2021-03-04 10:00:18 +0200

    подключил БД и добавил запросы в index.php

--- index.php
+++ index.php
@@ -4,57 +4,62 @@ require('./helpers.php');
 // показывать или нет выполненные задачи
 $show_complete_tasks = rand(0, 1);
 
-$projects = [
-    'Входящие',
-    'Учеба',
-    'Работа',
-    'Домашние дела',
-    'Авто'
-];
-$tasks = [
-    [
-        'task' => 'Собеседование в IT компании',
-        'date' => '01.12.2019',
-        'project' => 'Работа',
-        'done' => false
-    ],
-    [
-        'task' => 'Выполнить тестовое задание',
-        'date' => '25.12.2019',
-        'project' => 'Работа',
-        'done' => false
-    ],
-    [
-        'task' => 'Сделать задание первого раздела',
-        'date' => '21.12.2019',
-        'project' => 'Учеба',
-        'done' => true
-    ],
-    [
-        'task' => 'Встреча с другом',
-        'date' => '05.02.2021',
-        'project' => 'Входящие',
-        'done' => false
-    ],
-    [
-        'task' => 'Купить корм для кота',
-        'date' => null,
-        'project' => 'Домашние дела',
-        'done' => false
-    ],
-    [
-        'task' => 'Заказать пиццу',
-        'date' => null,
-        'project' => 'Домашние дела',
-        'done' => false
-    ],
-];
+// текущий пользователь
+$curent_user_id = 1;
+
+// подключаемся к БД
+$con = mysqli_connect("localhost", "root", "root", "doingsdone");
+if (!$con) {
+    print("Ошибка подключения к БД: " . mysqli_connect_error());
+}
+mysqli_set_charset($con, "utf8");
+
+// получаем из БД список проектов текущего пользователя
+
+$sql = "SELECT title FROM projects WHERE user_id = '$curent_user_id'";
+$result_sql = mysqli_query($con, $sql);
+
+$projects = [];
+
+if ($result_sql) {
+    $projects_sql = mysqli_fetch_all($result_sql);
+    foreach ($projects_sql as $project) {
+        array_push($projects, $project['0']);
+    }
+} else {
+    print("Ошибка подключения к БД: " . mysqli_connect_error());
+}
+
+// получаем из БД список задач текущего пользователя
+
+$sql = "SELECT t.title, time_end, p.title, is_done FROM tasks t JOIN projects p ON t.project_id = p.id WHERE t.user_id = '$curent_user_id'";
+$result_sql = mysqli_query($con, $sql);
+
+$tasks = [];
+$tasks_key = ['task', 'date', 'project', 'done'];
+
+if ($result_sql) {
+    $tasks_sql = mysqli_fetch_all($result_sql);
+    foreach ($tasks_sql as $task_sql) {
+        $task = array_combine($tasks_key, $task_sql);
+        if($task['date'] != "0000-00-00 00:00:00") {
+            $task['date'] = date("d.m.Y", strtotime($task['date']));
+        } else {
+            $task['date'] = null;
+        }
+        array_push($tasks, $task);
+    }
+} else {
+    print("Ошибка подключения к БД: " . mysqli_connect_error());
+}
+
 
 // подсчет количества задач
 
-function get_projects_count($tasks){
-    return array_reduce($tasks, function ($carry, $item){
-        if(!isset($carry[$item['project']])){
+function get_projects_count($tasks)
+{
+    return array_reduce($tasks, function ($carry, $item) {
+        if (!isset($carry[$item['project']])) {
             $carry += [$item['project'] => 1];
         } else {
             $carry[$item['project']] += 1;
