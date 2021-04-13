9e45196ac1f84318dd424717f81b7ae20278ec62
Kuzmin Roman <archer039@gmail.com>
2021-04-13 16:35:18 +0200

    перенёс получение данных для шаблонов в функции

--- functions/data.php
+++ functions/data.php
@@ -13,3 +13,41 @@ function get_main_data ($current_user_id)
     ];
     return $main_data;
 }
+
+// функция получения данных для шаблона form_task
+function get_form_task ($current_user_id)
+{
+    $form_task_data = get_main_data($current_user_id);
+    if (isset($errors)){
+        $form_task['errors'] = $errors;
+    }
+    return $form_task_data;
+}
+
+// функция получения данных для шаблона layout
+function get_layout_data ($current_user_id)
+{
+    $main_data = get_main_data($current_user_id);
+    $form_task_data = get_form_task($current_user_id);
+
+    // Данные для layout
+    $user_name = 'User';
+    $title_name = 'Дела в порядке';
+    $content = include_template('main.php', $main_data);
+
+    // проверка id выбранного проекта
+    check_selected_project_id($current_user_id);
+
+    // выбор страниц
+    if (isset($_GET['page']) && $_GET['page'] == 'add_task') {
+        $content = include_template('form_task.php', $form_task_data);
+    }
+
+
+    $layout_data = [
+        'user' => $user_name,
+        'title' => $title_name,
+        'content' => $content
+    ];
+    return $layout_data;
+}
--- index.php
+++ index.php
@@ -5,35 +5,9 @@ $current_user_id = 1;
 
 require_once (__DIR__ . '/functions/functions.php');
 
-
-
 // проверка id выбранного проекта
 check_selected_project_id($current_user_id);
 
-$main_data = get_main_data($current_user_id);
-
-// данные для form_task
-$form_task = $main_data;
-if (isset($errors)){
-    $form_task['errors'] = $errors;
-}
-
-
-// Данные для layout
-$user_name = 'User';
-$title_name = 'Дела в порядке';
-$content = include_template('main.php', $main_data);
-
-// выбор страниц
-if (isset($_GET['page']) && $_GET['page'] == 'add_task') {
-        $content = include_template('form_task.php', $form_task);
-}
-
-
-$layout_data = [
-    'user' => $user_name,
-    'title' => $title_name,
-    'content' => $content
-];
+$layout_data = get_layout_data($current_user_id);
 
 print(include_template('layout.php', $layout_data));
