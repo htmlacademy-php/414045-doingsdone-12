d17d8d8e931c1cce8504067f1df1a7c733495906
Kuzmin Roman <archer039@gmail.com>
2021-05-13 16:53:43 +0200

    удалил неиспользуемый код из script.js, упростил условие в change_task_state.php, установил composer

--- scripts/change_task_state.php
+++ scripts/change_task_state.php
@@ -4,7 +4,7 @@
  */
 require_once ('../bootstrap.php');
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['id_task_for_state_changing'])) {
+if (isset($_POST['id_task_for_state_changing'])) {
     change_task_done_state($user_id, $_POST['id_task_for_state_changing']);
     redirect_to_home();
 }
