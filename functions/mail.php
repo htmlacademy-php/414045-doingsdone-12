b694fbc21103182a1bb8187260a4ab29b672935d
Kuzmin Roman <archer039@gmail.com>
2021-05-19 10:44:45 +0200

    добавил файл config.php

--- functions/mail.php
+++ functions/mail.php
@@ -2,12 +2,12 @@
 
 function send_reminder_for_tasks($name, $email, $mail_content)
 {
-    $transport = (new Swift_SmtpTransport('smtp.timeweb.ru', 25))
-        ->setUsername('doingsdone@ci65243.tmweb.ru')
-        ->setPassword('9K8TpC8T');
+    $transport = (new Swift_SmtpTransport(SMTP_HOST, SMTP_PORT))
+        ->setUsername(SMTP_USER)
+        ->setPassword(SMTP_PASSWORD);
 
     $message = (new Swift_Message('Уведомление от сервиса «Дела в порядке»'))
-        ->setFrom(['doingsdone@ci65243.tmweb.ru' => 'Дела в порядке'])
+        ->setFrom([SMTP_USER => 'Дела в порядке'])
         ->setTo([$email => $name])
         ->setContentType("text/html")
         ->setBody($mail_content);
